2604925d624c8be6875d1fd9a50330cbeecfea59
AntonLachin <lachin-1993@mail.ru>
2022-06-23 21:29:15 +0300

    1st exercise

---
+++ src/Homework3/Homework3.java
@@ -0,0 +1,9 @@
+package Homework3;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        Study JavaProgram = new Study("Изучение Java - это просто!");
+        System.out.println(JavaProgram);
+    }
+}
+
---
+++ src/Homework3/Study.java
@@ -0,0 +1,13 @@
+package Homework3;
+
+public class Study {
+    private String course;
+
+    public Study(String course) {
+        this.course = course;
+    }
+
+    public String toString() {
+        return course;
+    }
+}
